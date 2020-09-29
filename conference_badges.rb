3cf8e439d0818085b33aaecc36df73e510c87335
Mike Barberry <mikebarberry@protonmail.com>
2020-09-29 04:22:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  array.map do |element|
+    "Hello, my name is #{element}."
+  end
+end
+
+batch_badge_creator(speakers)
