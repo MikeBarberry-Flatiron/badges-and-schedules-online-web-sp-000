d3fa2487e96655e58914843542e9df10b668ce2c
Mike Barberry <mikebarberry@protonmail.com>
2020-09-29 04:56:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-
-  array.collect do |name, index|
+  array.each_with_index.map do |name, index|
     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
