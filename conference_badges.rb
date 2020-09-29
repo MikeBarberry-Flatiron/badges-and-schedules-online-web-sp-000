7db05b8db93633d4a08cf635e85f1880fbaa0dc4
Mike Barberry <mikebarberry@protonmail.com>
2020-09-29 05:23:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,9 +17,12 @@ def assign_rooms(array)
   end
 end
 
-def printer(array)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
-end
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
 
-printer(speakers)
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
